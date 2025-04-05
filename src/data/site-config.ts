609ded27099d4b64abc97683ad0d068ea8a1e906
Kevin Villafuerte <nivek9021@gmail.com>
2025-04-05 14:59:59 -0500

    update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -94,15 +94,15 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hola y bienvenido a mi rincón de la web!.',
+        text: "Soy **Kevin Alfonso Villafuerte Sanchez**, Analista de Sistemas y estadistico en el Centro de Salud Mental Comunitario Santiago, dedicado a los ámbitos de la creacion de reportes estadistico, creacion y mantenimiento de sistemas y aplicaciones para el manejo de informacion de los usuarios y personal de la salud mental. Mi enfoque se basa en la intuición, la investigación justa y el uso de la estética como catalizador para crear productos excepcionales. Aprecio profundamente el software de alta gama, el diseño visual y los principios del crecimiento impulsado por el producto. Explora algunos de mis proyectos de programación en <a href='https://github.com/vsksv'>GitHub</a>.",
         image: {
             src: '/vs.jpg',
-            alt: 'A person sitting at a desk in front of a computer'
+            alt: 'A person sitting at a desk in front of a computer in a mental health ward'
         },
         actions: [
             {
-                text: 'Get in Touch',
+                text: 'Ponte en Contacto',
                 href: '/contact'
             }
         ]
